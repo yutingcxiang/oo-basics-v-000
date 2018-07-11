5c52808ee6c74ed64536be6943f29187937d52d9
Christine Xiang <yutingcxiang@gmail.com>
2018-07-11 05:33:59 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,6 @@
 class Book
+  attr_accessor :author, :page_count, :genre
+  
   def initialize(title)
     @title = title
   end
@@ -6,31 +8,7 @@ class Book
   def title
     @title
   end
-  
-  def author=(author)
-    @author = author
-  end
-  
-  def author
-    @author
-  end
-  
-  def page_count=(num)
-    @page_count = num
-  end
-  
-  def page_count
-    @page_count
-  end
-  
-  def genre=(genre)
-    @genre = genre
-  end
-  
-  def genre
-    @genre
-  end
-  
+
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
